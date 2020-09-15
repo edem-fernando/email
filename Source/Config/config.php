d713791c1609ca3bcd2022635aac4d9693090af4
edem fernando <edem.fbc@gmail.com>
2020-09-15 20:48:52 -0300

    Seconds Commit

--- Source/Config/config.php
+++ Source/Config/config.php
@@ -1,12 +1,12 @@
 <?php
 
 // MAIL
-define("CONF_MAIL_HOST", "smtp-relay.sendinblue.com");
-define("CONF_MAIL_PORT", "587");
-define("CONF_MAIL_USER", "edem.bastos@acad.ifma.edu.br");
-define("CONF_MAIL_PASS", "hKsPSfNO1A580nz6");
-define("CONF_MAIL_SENDER", ["name" => "Edem Fernando", "address" => "edem.bastos@acad.ifma.edu.br"]);
-define("CONF_MAIL_SUPPORT", "edem.fernando@hotmail.com");
+define("CONF_MAIL_HOST", "seu host de email");
+define("CONF_MAIL_PORT", "porta");
+define("CONF_MAIL_USER", "user host");
+define("CONF_MAIL_PASS", "password");
+define("CONF_MAIL_SENDER", ["name" => "Seunome", "address" => "seu_email"]);
+define("CONF_MAIL_SUPPORT", "seu_email_de_supporte");
 define("CONF_MAIL_OPTION_LANG", "br");
 define("CONF_MAIL_OPTION_HTML", true);
 define("CONF_MAIL_OPTION_AUTH", true);
